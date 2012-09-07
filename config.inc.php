9c8713a4391ac6f65ecc4a72cf1cb0b92df72ce1
unknown <20111224i705@20111224i705-PC.(none)>
2012-09-07 14:11:31 +0800

    Signed-off-by: unknown <20111224i705@20111224i705-PC.(none)>

--- config.inc.php
+++ config.inc.php
@@ -2,10 +2,10 @@
 
     return array(
         'DB_TYPE'=>'mysql',
-        'DB_HOST'=>'localhost',
+        'DB_HOST'=>'192.168.1.231',
         'DB_NAME'=>'myerp',
-        'DB_USER'=>'root',
-        'DB_PWD'=>'',
+        'DB_USER'=>'erp',
+        'DB_PWD'=>'travelERP@234',
         'DB_PORT'=>3306,
         'DB_PREFIX'=>'',
         'APP_DEBUG'=>false,
