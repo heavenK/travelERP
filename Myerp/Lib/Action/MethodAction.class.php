0a3897e56c20ac817859652b62cdbbcfdcf683e1
unknown <20111224i705@20111224i705-PC.(none)>
2012-09-25 11:15:41 +0800

    Signed-off-by: unknown <20111224i705@20111224i705-PC.(none)>

--- Myerp/Lib/Action/MethodAction.class.php
+++ Myerp/Lib/Action/MethodAction.class.php
@@ -553,7 +553,7 @@ class MethodAction extends CommonAction{
 	//同步DataOM:售价开放管理到对象
      public function _shoujiaToDataOM($data) {
 		$DataOM = D("DataOM");
-		$DataOM->where("`dataID` = '$data[chanpinID]' and `datatype` = '售价'")->delete();
+		//$DataOM->where("`dataID` = '$data[chanpinID]' and `datatype` = '售价'")->delete();
 		$OM['dataID'] = $data['chanpinID'];
 		$OM['datatype'] = '售价';
 		$OM['type'] = '开放';
