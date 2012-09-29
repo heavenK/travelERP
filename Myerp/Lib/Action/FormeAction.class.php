c1da333cf236b5c72bc26c2473f865e905fa7e99
unknown <20111224i705@20111224i705-PC.(none)>
2012-09-29 11:11:43 +0800

    Signed-off-by: unknown <20111224i705@20111224i705-PC.(none)>

--- Myerp/Lib/Action/FormeAction.class.php
+++ Myerp/Lib/Action/FormeAction.class.php
@@ -1110,9 +1110,15 @@ class FormeAction extends Action{
 			$data['bumen_copy'] = $bumen['title'];
 			//订单状态
 			$data['status'] = '确认';
-			if($v['check_status'] == '回收站' || $v['check_status'] == '审核不通过' || ((strtotime($v['chutuanriqi']) < time()) && $v['check_status'] == '等待审核')){
+			if($v['check_status'] == '回收站' || $v['check_status'] == '审核不通过'){
 				$data['status'] = '候补';
 				$data['status_system'] = -1;
+			}
+			if(strtotime($v['chutuanriqi']) < time()){
+				if($v['check_status'] == '等待审核' || $v['check_status'] == '准备'){
+				$data['status'] = '候补';
+				$data['status_system'] = -1;
+				}
 			}
 			if($data['dingdan']['lianxiren'] == '')
 			$data['dingdan']['lianxiren'] = 0;
