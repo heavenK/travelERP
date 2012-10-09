ca5f5ad7dc651d7db2a666c90da3a916aa8f88db
unknown <20111224i705@20111224i705-PC.(none)>
2012-10-09 17:12:02 +0800

    Signed-off-by: unknown <20111224i705@20111224i705-PC.(none)>

--- Myerp/Lib/Action/IndexAction.class.php
+++ Myerp/Lib/Action/IndexAction.class.php
@@ -85,7 +85,7 @@ class IndexAction extends Action{
 	
     public function verify() {  
         $type = isset($_GET['type']) ? $_GET['type'] : 'gif'; 
-        import("@.ORG.Image"); 
+        import("@.ORG.Util.Image"); 
 		Image::buildImageVerify(4, 1, $type); 
     } 	
 	
