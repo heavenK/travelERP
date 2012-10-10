aa53c1f90db3d405055a946d67face11c9b7dbc1
unknown <20111224i705@20111224i705-PC.(none)>
2012-10-10 11:13:16 +0800

    Signed-off-by: unknown <20111224i705@20111224i705-PC.(none)>

--- Myerp/Common/NewFunction.php
+++ Myerp/Common/NewFunction.php
@@ -132,8 +132,8 @@ function mb_unserialize($serial_str) {
 }	
 
 function simple_unserialize($serial_str) {
-    $serial_str= preg_replace('!s:(\d+):"(.*?)";!se', '"s:".strlen("$2").":\"$2\";"', $serial_str );//格式不移除
-    $serial_str= str_replace("\r", "", $serial_str);      
+//    $serial_str= preg_replace('!s:(\d+):"(.*?)";!se', '"s:".strlen("$2").":\"$2\";"', $serial_str );//格式不移除
+//    $serial_str= str_replace("\r", "", $serial_str);      
     return unserialize($serial_str);
 }	
 
