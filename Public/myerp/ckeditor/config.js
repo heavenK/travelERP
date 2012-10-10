5772cc56a01642b2981ad8d55824f40281d81a8f
unknown <20111224i705@20111224i705-PC.(none)>
2012-10-10 14:55:02 +0800

    Signed-off-by: unknown <20111224i705@20111224i705-PC.(none)>

--- Public/myerp/ckeditor/config.js
+++ Public/myerp/ckeditor/config.js
@@ -7,22 +7,5 @@ CKEDITOR.editorConfig = function( config )
 {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
-	 config.uiColor = '#F6F6F6';
-	
-	config.toolbar = 'MXICToolbar';
-    config.toolbar_MXICToolbar =
-    [
-    ['Source','Templates','PasteFromWord','Find','Replace','RemoveFormat'],
-    ['Bold','Italic','Underline','Strike','-'],
-    ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote'],
-    ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
-    ['Link','Unlink','Anchor'],
-    ['Image','Table','HorizontalRule','SpecialChar','PageBreak'],
-    ['Styles','Format','Font','FontSize'],
-    ['TextColor','BGColor'],
-    ['Maximize','ShowBlock'],
-    ];
-    config.width = 900;
-    config.height = 150;	
-
+	// config.uiColor = '#AADC6E';
 };
