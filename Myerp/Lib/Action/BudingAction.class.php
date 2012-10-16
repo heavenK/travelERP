8a77166db607730ff16a8313c16fd34c2d69e9e6
unknown <20111224i705@20111224i705-PC.(none)>
2012-10-16 16:27:21 +0800

    Signed-off-by: unknown <20111224i705@20111224i705-PC.(none)>

--- Myerp/Lib/Action/BudingAction.class.php
+++ Myerp/Lib/Action/BudingAction.class.php
@@ -57,6 +57,64 @@ class BudingAction extends Action{
     }
 	
 	
+	//报账项字段填充，报账标题，子团日期，子团团号，子团标题
+    public function baozhangxiangtianchong() {
+		C('TOKEN_ON',false);
+		echo "报账项字段填充，报账标题，子团日期，子团团号，子团标题";
+		$ViewBaozhangitem = D("ViewBaozhangitem");
+		$ViewBaozhang = D("ViewBaozhang");
+		$ViewZituan = D("ViewZituan");
+		$ViewDJtuan = D("ViewDJtuan");
+		$Chanpin = D("Chanpin");
+		$all = $ViewBaozhangitem->findall();
+		foreach($all as $v){
+			$data = $v;
+			$data['baozhangitem'] = $v;
+			//获得报账单
+			$baozhang = $ViewBaozhang->where("`chanpinID` = $v[parentID]")->find();
+			$data['baozhangitem']['baozhangtitle_copy'] = $zituan['title_copy'];
+			//获得团
+			$cp = $Chanpin->where("`chanpinID` = '$baozhang[parentID]'")->find();
+			if($cp['marktype'] == 'zituan'){
+				$zituan = $ViewZituan->where("`chanpinID` = '$cp[chanpinID]'")->find();
+				$data['baozhangitem']['tuantitle_copy'] = $zituan['title_copy'];
+				$data['baozhangitem']['tuanqi_copy'] = $zituan['chutuanriqi'];
+				$data['baozhangitem']['tuanhao_copy'] = $zituan['tuanhao'];
+					
+			}
+			if($cp['marktype'] == 'DJtuan'){
+				$zituan = $ViewZituan->where("`chanpinID` = '$cp[chanpinID]'")->find();
+				$data['baozhangitem']['tuantitle_copy'] = $zituan['title'];
+				$data['baozhangitem']['tuanqi_copy'] = $zituan['jietuantime'];
+				$data['baozhangitem']['tuanhao_copy'] = $zituan['tuanhao'];
+			}
+			$Chanpin->myRcreate($data);
+		}
+		echo "结束";
+    }
+	
+	
+	//线路联合体开放给自己
+    public function lianhetixianlukaifang() {
+		C('TOKEN_ON',false);
+		echo "线路联合体开放给自己om";
+		$ViewXianlu = D("ViewXianlu");
+		$DataOM = D("DataOM");
+		$Chanpin = D("Chanpin");
+		$all = $ViewXianlu->findall();
+		foreach($all as $v){
+			$data['dataID'] = $v['chanpinID'];
+			$data['datatype'] = '线路';
+			$data['type'] = '管理';
+			$data['DUR'] = $v['departmentID'].',,';
+			$ish = $DataOM->where($data)->find();
+			if(!$ish)
+			$DataOM->add($data);
+		}
+		echo "结束";
+    }
+	
+	
 	
 }
 ?>
