2450a7b888cd3a33986678ca604f62a1e6da1874
unknown <20111224i705@20111224i705-PC.(none)>
2013-03-25 09:54:37 +0800

    Signed-off-by: unknown <20111224i705@20111224i705-PC.(none)>

--- Myerp/Lib/Action/ChanpinAction.class.php
+++ Myerp/Lib/Action/ChanpinAction.class.php
@@ -1033,7 +1033,7 @@ class ChanpinAction extends CommonAction{
 	public function tongji() {
 		A("Method")->showDirectory("统计");
 		//搜索
-		$where_unit['status_baozhang'] = '批准';
+		$where['status_baozhang'] = '批准';
 		
 		if($_REQUEST['title'])
 			$where_unit['title'] = array('like','%'.$_REQUEST['title'].'%');
