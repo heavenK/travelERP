3b861209f6e54bf8320878f7928f84fa7f23bcae
unknown <20111224i705@20111224i705-PC.(none)>
2013-09-02 10:04:06 +0800

    Signed-off-by: unknown <20111224i705@20111224i705-PC.(none)>

--- Myerp/Lib/Action/MethodAction.class.php
+++ Myerp/Lib/Action/MethodAction.class.php
@@ -82,12 +82,9 @@ class MethodAction extends CommonAction{
 			$where['datatype'] = $datatype;
 		}
 		if($datatype == '地接'){
-			//优化查询
-			if($where){
 				$class_name = 'OMViewDJtuan';
 				$where = $this->_orderDijie($where,$datatype);
 				$order = 'jietuantime desc';
-			}
 			$where['datatype'] = $datatype;
 		}
 		if($datatype == '报账单'){
