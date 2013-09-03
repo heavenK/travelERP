1fd3dfa6a6a3bc226c887d0c0efa7da797c5bc08
unknown <20111224i705@20111224i705-PC.(none)>
2013-09-03 17:59:23 +0800

    Signed-off-by: unknown <20111224i705@20111224i705-PC.(none)>

--- Myerp/Lib/Action/MethodAction.class.php
+++ Myerp/Lib/Action/MethodAction.class.php
@@ -4559,11 +4559,6 @@ class MethodAction extends CommonAction{
 				$bzditem = $ViewBaozhangitem->relation('baozhanglist')->where("`chanpinID` = '$dataID'")->find();
 				$bzdi_pid = $bzditem['baozhanglist']['parentID'];
 				$cp_bzd = $Chanpin->where("`chanpinID` = '$bzdi_pid'")->find();
-				dump($cp_bzd);
-				dump($Chanpin);
-				$cp = $Chanpin->where("`chanpinID` = '$cp_bzd[parentID]'")->find();
-				dump($cp);
-				dump($Chanpin);
 				if($cp['marktype'] == 'zituan'){
 					$role = '计调';
 					$type = '组团';
