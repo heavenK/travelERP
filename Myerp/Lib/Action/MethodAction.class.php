620e70d2f631b247aba6d046007c8d6d1f143e3f
unknown <20111224i705@20111224i705-PC.(none)>
2013-09-03 17:58:22 +0800

    Signed-off-by: unknown <20111224i705@20111224i705-PC.(none)>

--- Myerp/Lib/Action/MethodAction.class.php
+++ Myerp/Lib/Action/MethodAction.class.php
@@ -4559,6 +4559,8 @@ class MethodAction extends CommonAction{
 				$bzditem = $ViewBaozhangitem->relation('baozhanglist')->where("`chanpinID` = '$dataID'")->find();
 				$bzdi_pid = $bzditem['baozhanglist']['parentID'];
 				$cp_bzd = $Chanpin->where("`chanpinID` = '$bzdi_pid'")->find();
+				dump($cp_bzd);
+				dump($Chanpin);
 				$cp = $Chanpin->where("`chanpinID` = '$cp_bzd[parentID]'")->find();
 				dump($cp);
 				dump($Chanpin);
