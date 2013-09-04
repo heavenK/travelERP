e2331a358d7f83dfce2989a268757cdb33fd21cf
unknown <20111224i705@20111224i705-PC.(none)>
2013-09-04 15:44:55 +0800

    Signed-off-by: unknown <20111224i705@20111224i705-PC.(none)>

--- Myerp/Lib/Action/MethodAction.class.php
+++ Myerp/Lib/Action/MethodAction.class.php
@@ -4558,6 +4558,8 @@ class MethodAction extends CommonAction{
 					if(!$dataOMlist){
 						$bzd = $Chanpin->where("`chanpinID` = '$dataID'")->find();
 						$cp = $Chanpin->where("`chanpinID` = '$bzd[parentID]'")->find();
+						dump($bzd);
+						dump($Chanpin);
 						dump($cp);
 						if($cp){
 							if($cp['marktype'] == 'zituan'){
