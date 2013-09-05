ff532f4a4fc0f1b49d18ffe000aa2022a4d52d7b
unknown <20111224i705@20111224i705-PC.(none)>
2013-09-05 14:16:09 +0800

    Signed-off-by: unknown <20111224i705@20111224i705-PC.(none)>

--- Myerp/Lib/Action/MethodAction.class.php
+++ Myerp/Lib/Action/MethodAction.class.php
@@ -4518,15 +4518,12 @@ class MethodAction extends CommonAction{
 				}
 				if($datatype == '线路'){
 					//修复产品部门
-							dump($d_cp);
 					if($d_cp['bumen_copy'] == '总经理' || $d_cp['bumen_copy'] == '大连古莲国际旅行社'){
 						$d_list = $this->_getDURlist_name($user_name,'','组团');	
-							dump($d_list);
 						//随即第一个
-						$d_cp['departmentID'] = $d_list[0];	
+						$d_cp['departmentID'] = $d_list[0]['bumenID'];	
 						if(false !== $Chanpin->mycreate($d_cp))
 							$departmentID = $d_cp['departmentID'];
-							dump($departmentID);
 					}
 					if(!$dataOMlist){
 						$dataOMlist = $this->_setDataOMlist('计调','组团',$user_name,$departmentID);
