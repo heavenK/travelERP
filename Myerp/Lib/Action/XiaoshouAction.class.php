818ad605065c353297d28ab6d2afa2a3526b2537
p308715744 <p3087157442000@yahoo.com.cn>
2013-09-30 14:58:28 +0800

    Signed-off-by: p308715744 <p3087157442000@yahoo.com.cn>

--- Myerp/Lib/Action/XiaoshouAction.class.php
+++ Myerp/Lib/Action/XiaoshouAction.class.php
@@ -88,6 +88,9 @@ class XiaoshouAction extends Action{
 		$xianlu = $DataCopy->where("`dataID` = '$_REQUEST[xianluID]' and `datatype` = '线路'")->order("time desc")->find();
 		$xianlu = simple_unserialize($xianlu['copy']);
 		$xianlu['xianlu_ext'] = simple_unserialize($xianlu['xianlu']['xianlu_ext']);
+		if(!$xianlu['xingcheng']){
+			$xianlu['xianlu'] = $Chanpin->relationGet("xingchenglist");
+		}
 		//解析视频
 		$ViewDataDictionary = D("ViewDataDictionary");
 		$nameshipin = $xianlu['xianlu']['shipin'];
