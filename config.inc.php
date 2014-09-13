5e3d1e117c887b6664173d68042eb4869576d43c
heavenK <heaven@we54.com>
2014-09-13 11:23:21 +0800

    Signed-off-by: heavenK <heaven@we54.com>

--- config.inc.php
+++ config.inc.php
@@ -2,7 +2,7 @@
 
     return array(
         'DB_TYPE'=>'mysql',
-        'DB_HOST'=>'localhost',
+        'DB_HOST'=>'127.0.0.1',
         'DB_NAME'=>'myerp',
         'DB_USER'=>'root',
         'DB_PWD'=>'',
