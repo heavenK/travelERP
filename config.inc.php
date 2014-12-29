bafcb2902e863d09df0e1c0a04c5a4387ba42521
neconano <308715744@qq.com>
2014-12-29 19:06:51 +0800

    Revert "a"
    This reverts commit edf6e628ba9702647671c0dfe9ea82bdaaf2d10d.

--- config.inc.php
+++ config.inc.php
@@ -2,12 +2,11 @@
 
     return array(
         'DB_TYPE'=>'mysql',
-        'DB_HOST'=>'localhost',
+        'DB_HOST'=>'127.0.0.1',
         'DB_NAME'=>'myerp',
         'DB_USER'=>'root',
         'DB_PWD'=>'',
         'DB_PORT'=>3306,
         'DB_PREFIX'=>'',
-        'APP_DEBUG'=>false,
     );
     ?>
--- index.php
+++ index.php
@@ -1,21 +1,24 @@
 <?php
 define('IN_ET', TRUE);
 require('./define.inc.php');
+require('./define2.inc.php');
 
 define('ET_ROOT', dirname(__FILE__));
 define('MODE_NAME','mycore');
-define('THINK_PATH','../ThinkPHP/');
+//define('THINK_PATH','./ThinkPHP/');
+define('THINK_PATH',dirname(__FILE__).'/../ThinkPHP/');
 define('APP_NAME', 'Myerp');
 define('APP_PATH', './Myerp/');
 define('DEFAULT_TYPE','default');
 define('__PUBLIC__',ET_URL."Public");
-define('SITE_URL',ET_URL);
-define('APP_DEBUG', true);
+define('APP_DEBUG', false);//调试模式（无缓存）
+//define('APP_DEBUG', true);//调试模式（无缓存）
 
 require(APP_PATH.'Common/Function.php');
 require(APP_PATH.'Common/MyFunction.php');
 require(APP_PATH.'Common/FusionCharts.php');
 require(APP_PATH.'Common/NewFunction.php');
+require(APP_PATH.'Common/B2CFunction.php');
 
 require(THINK_PATH.'ThinkPHP.php');
 
