d6005dc9f5ea58ec33b224b03c1a03028d399220
neconano <308715744@qq.com>
2014-12-29 15:22:46 +0800

    a
    a

--- Myerp/Lib/Api/PHPExcelAction.class.php
+++ Myerp/Lib/Api/PHPExcelAction.class.php
@@ -23,7 +23,9 @@ class PHPExcelAction extends Action {
         $where = A("Method")->_facade($class_name,$where);//过滤搜索项
         //$chanpin = D($class_name)->relation($relation)->where($where)->order($order)->select();
         $chanpin = D($class_name)->where($where)->order($order)->select();
+        dump(123123123);
         dump($chanpin);
+        exit;
         return  $chanpin;
     }
 
