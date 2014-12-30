b6f01aba0e583ba8191e4b912ed247ddcb22edbb
neconano <308715744@qq.com>
2014-12-30 11:47:06 +0800

    a

--- index.php
+++ index.php
@@ -5,14 +5,14 @@ require('./define2.inc.php');
 
 define('ET_ROOT', dirname(__FILE__));
 define('MODE_NAME','mycore');
-//define('THINK_PATH','./ThinkPHP/');
+//define('THINK_PATH','../ThinkPHP/');
 define('THINK_PATH',dirname(__FILE__).'/../ThinkPHP/');
 define('APP_NAME', 'Myerp');
 define('APP_PATH', './Myerp/');
 define('DEFAULT_TYPE','default');
 define('__PUBLIC__',ET_URL."Public");
-define('APP_DEBUG', false);//调试模式（无缓存）
-//define('APP_DEBUG', true);//调试模式（无缓存）
+//define('APP_DEBUG', false);//调试模式（无缓存）
+define('APP_DEBUG', true);//调试模式（无缓存）
 
 require(APP_PATH.'Common/Function.php');
 require(APP_PATH.'Common/MyFunction.php');
