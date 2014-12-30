b3d1563fbf762cf6122b0a3043f623f41318785e
neconano <308715744@qq.com>
2014-12-30 15:29:27 +0800

    a

--- Myerp/Lib/Api/PHPExcelAction.class.php
+++ Myerp/Lib/Api/PHPExcelAction.class.php
@@ -1,12 +1,9 @@
 <?php
-
-
 class PHPExcelAction extends Action {
    
 
 
     public function _myinit() {
-        
      }
 
 
@@ -74,11 +71,79 @@ class PHPExcelAction extends Action {
 
 
 
+    public function wirteToExcel_21($data,$filename='') {
+            ob_end_clean();
+            error_reporting(E_ALL);
+            ini_set('display_errors', TRUE);
+            ini_set('display_startup_errors', TRUE);
+            date_default_timezone_set('Europe/London');
+
+            if (PHP_SAPI == 'cli')
+                die('This example should only be run from a Web Browser');
+
+            /** Include PHPExcel */
+            import('Lib.ORG.PHPExcel.Classes.PHPExcel',APP_PATH,'.php');
+
+
+            // Create new PHPExcel object
+            $objPHPExcel = new PHPExcel();
+
+            // Set document properties
+            $objPHPExcel->getProperties()->setCreator("Maarten Balliauw")
+                                         ->setLastModifiedBy("Maarten Balliauw")
+                                         ->setTitle("Office 2007 XLSX Test Document")
+                                         ->setSubject("Office 2007 XLSX Test Document")
+                                         ->setDescription("Test document for Office 2007 XLSX, generated using PHP classes.")
+                                         ->setKeywords("office 2007 openxml php")
+                                         ->setCategory("Test result file");
+
+
+            // Add some data
+            $objPHPExcel->setActiveSheetIndex(0)
+                        ->setCellValue('A1', 'Hello')
+                        ->setCellValue('B2', 'world!')
+                        ->setCellValue('C1', 'Hello')
+                        ->setCellValue('D2', 'world!');
+
+            // Miscellaneous glyphs, UTF-8
+            $objPHPExcel->setActiveSheetIndex(0)
+                        ->setCellValue('A4', 'Miscellaneous glyphs')
+                        ->setCellValue('A5', 'éàèùâêîôûëïüÿäöüç');
+
+            // Rename worksheet
+            $objPHPExcel->getActiveSheet()->setTitle('Simple');
+
+
+            // Set active sheet index to the first sheet, so Excel opens this as the first sheet
+            $objPHPExcel->setActiveSheetIndex(0);
+
+
+            // Redirect output to a client’s web browser (Excel5)
+            header('Content-Type: application/vnd.ms-excel');
+            header('Content-Disposition: attachment;filename="01simple.xls"');
+            header('Cache-Control: max-age=0');
+            // If you're serving to IE 9, then the following may be needed
+            header('Cache-Control: max-age=1');
+
+            // If you're serving to IE over SSL, then the following may be needed
+            header ('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
+            header ('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT'); // always modified
+            header ('Cache-Control: cache, must-revalidate'); // HTTP/1.1
+            header ('Pragma: public'); // HTTP/1.0
+
+            $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');
+            $objWriter->save('php://output');
+            exit;
+
+    }
+
+
 
 
 
     public function wirteToExcel_2($data,$filename='') {
         
+        ob_end_clean();
         Vendor ( 'Excel.PHPExcel' );
 
         // Create new PHPExcel object
@@ -111,6 +176,9 @@ class PHPExcelAction extends Action {
         // Set active sheet index to the first sheet, so Excel opens this as the first sheet
         $objPHPExcel->setActiveSheetIndex(0);
 
+       // dump($keylist);
+        //dump($datalist);
+//exit;
         // Redirect output to a client’s web browser (Excel5)
         header('Content-Type: application/vnd.ms-excel');
         header('Content-Disposition: attachment;filename="01simple.xls"');
