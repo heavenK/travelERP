09999504b06692184f5aff219ceb2e5539814ca7
heavenK <heaven_k0001@163.com>
2015-01-05 14:22:21 +0800

    Signed-off-by: heavenK <heaven_k0001@163.com>

--- Myerp/Lib/Action/XiaoshouAction.class.php
+++ Myerp/Lib/Action/XiaoshouAction.class.php
@@ -327,7 +327,6 @@ class XiaoshouAction extends Action{
 				$this->ajaxReturn($_REQUEST,'错误,订单人数超出剩余，请联系计调！', 0);
 			}
 			
-			$this->ajaxReturn($_REQUEST,$shengyurenshu, 0);
 			
 			
 			//价格范围
@@ -336,7 +335,6 @@ class XiaoshouAction extends Action{
 			if($shoujia['childprice'] - $shoujia['cut'] > $_REQUEST['childprice'])
 				$this->ajaxReturn($_REQUEST,'错误,儿童售价超过可折扣范围！', 0);
 		}
-		$this->ajaxReturn($_REQUEST,$chanpintype, 0);
 		//检查人数
 		if($_REQUEST['chengrenshu'] + $_REQUEST['ertongshu'] + $_REQUEST['lingdui_num'] <= 0 )
 			$this->ajaxReturn($_REQUEST,'错误,订单人数必须大于0！', 0);
